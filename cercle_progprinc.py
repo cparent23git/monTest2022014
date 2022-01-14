0b46b5ad563a67c20b0140153cffe68c42928b43
cparent23git <claude.parent.23@gmail.com>
2022-01-14 13:43:56 -0500

    Test: Changements: nouveau commentaire et nouvelle ligne

--- cercle_progprinc.py
+++ cercle_progprinc.py
@@ -4,8 +4,9 @@ cyl = Cylindre(5,7)
 print("{0:.2f}".format(Cercle.surface(cyl)))
 print("{0:.2f}".format(Cylindre.surface(cyl)))
 print("{0:.2f}".format(cyl.surface()))
-print("{0:.2f}".format(cyl.volume()))
+print("{0:.2f}".format(cyl.volume())) #essai
 
 co = Cone(5,7)
 print("{0:.2f}".format(co.volume()))
 # print(f"De cercle_progprinc:  __name__ == {__name__}.")
+print("une nouvelle ligne")
