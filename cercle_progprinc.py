2f4b2a7ec744d996041222ed75759e732e5518e1
cparent23git <claude.parent.23@gmail.com>
2022-01-14 13:38:13 -0500

    Test: Mon premier commit

---
+++ cercle_progprinc.py
@@ -0,0 +1,11 @@
+from cercle_module import *
+
+cyl = Cylindre(5,7)
+print("{0:.2f}".format(Cercle.surface(cyl)))
+print("{0:.2f}".format(Cylindre.surface(cyl)))
+print("{0:.2f}".format(cyl.surface()))
+print("{0:.2f}".format(cyl.volume()))
+
+co = Cone(5,7)
+print("{0:.2f}".format(co.volume()))
+# print(f"De cercle_progprinc:  __name__ == {__name__}.")
